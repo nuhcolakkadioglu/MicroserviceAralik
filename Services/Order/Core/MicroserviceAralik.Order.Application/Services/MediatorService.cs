bf7d480e4e8ef96f32bb262b3d2186c380a85a42
Nuh <n.colakkadioglu@gmail.com>
2024-12-11 10:49:44 +0300

    add MediatorService

---
+++ Services/Order/Core/MicroserviceAralik.Order.Application/Services/MediatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroserviceAralik.Order.Application.Services;
+public static class MediatorService
+{
+    public static void AddMed(this  IServiceCollection services)
+    {
+        services.AddMediatR(opt => opt.RegisterServicesFromAssembly(typeof(MediatorService).Assembly));
+
+    }
+}
