ba6d6f66c994ed62ceff18653558adf6498b4862
Nuh <n.colakkadioglu@gmail.com>
2024-12-11 14:32:19 +0300

    add entity

---
+++ Services/Cargo/MicroserviceAralik.Services.Cargo.EntityLayer/Concrete/CargoDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceAralik.Services.Cargo.EntityLayer.Concrete;
+public class CargoDetail
+{
+    public int Id { get; set; }
+    public int SenderCustomerId { get; set; }
+    public int ReceiverCustomerId { get; set; }
+    public string Barcode { get; set; }
+    public string CompanyId { get; set; }
+    public Company Company { get; set; }
+
+    public Customer SenderCustomer { get; set; }
+    public Customer ReceiverCustomer { get; set; }
+}
---
+++ Services/Cargo/MicroserviceAralik.Services.Cargo.EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceAralik.Services.Cargo.EntityLayer.Concrete;
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string District { get; set; }
+    public string City { get; set; }
+    public string UserCostomerId { get; set; }
+    public List<CargoDetail> SentCargoDetails { get; set; }
+    public List<CargoDetail> ReceivedCargoDetails { get; set; }
+}
